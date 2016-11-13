c2e64399c057ed30ad1baa3fb65c231f7e362d47
Alan Evans <alan.evans@redspace.com>
2016-11-13 10:08:33 -0400

    Use linear, can get sharp still using custom nearest function

--- js/main.js
+++ js/main.js
@@ -70,8 +70,8 @@ function init() {
     nesTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, nesTexture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, tempCanvas);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 
     render();
 
